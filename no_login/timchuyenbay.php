2027bf2e4690a1967b209ba9c742bd70b36a4642
T10kg <ptkenzy@gmail.com>
2023-11-07 08:37:50 -0800

    ccc

--- no_login/timchuyenbay.php
+++ no_login/timchuyenbay.php
@@ -1,4 +1,9 @@
-
+<?php
+require("../conn.php");
+require("../client/func.php");
+$city = cityname($conn);
+$cityy = cityname($conn); 
+?>
 
 <form method="post">
     <label>Tìm chuyến bay:</label><br><br>
