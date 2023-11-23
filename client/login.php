95735e835926ae6290675d8965040420dd289d7d
tuvipvannoi <tqhungktpm2211072@student.ctuet.edu.vn>
2023-11-23 23:27:09 +0700

    ccne

--- client/login.php
+++ client/login.php
@@ -1,20 +1,26 @@
 <html><link rel="stylesheet" href="../css/login.css" />
 <body>
-<div class="banner"><img src="../img/sk4.jpg" alt="" width="75%">
-<div class="textt">
-<div class="text">
-    Đăng nhập
-      </div>
-<div class="container">
+<div class="banner"><img src="../img/rst.webp" alt="" width="75%">
+    <div class="textt">
+        <div class="text">
+        Đăng nhập
+        </div>
+        <div class="container">
         <div class="top">
-    <form method="post" action="logincheck.php">
-        <label for="username">Tên đăng nhập:</label>
-        <input type="text" id="username" name="username" required><br><br>
-        <label for="password">Mật khẩu:</label>
-        <input type="password" id="password" name="password" required><br><br>
-        <input type="submit" value="Đăng nhập" name="submit">
-    </form>
-       <a href="register.php"><button>Creart my accout</button> </a>
-    </div> </div></div>
+        <form method="post" action="logincheck.php">
+            <div class="topp">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br><br>
+             </div>
+             <div class="topp">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            </div>
+            <input type="submit" value="Đăng nhập" name="submit">
+        </form>
+        <a href="register.php"><button>Creart my accout</button> </a>
+        </div> 
+    </div>
+</div>
     </body>
 </html>
